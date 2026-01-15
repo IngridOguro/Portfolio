f158ca74701fc2cad840aa46c3a810ac3eabcda6
IS Oguro <ingrid.shizue@gmail.com>
2026-01-15 16:33:36 -0300

    Adicionando fontfamily, estruturando seção experiência,.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../bootstrap/navbar/navbar';
+import { Hero } from '../sections/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
