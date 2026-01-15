6e17b7b7b2900a57d9918faadebbcb6fc21da92a
IS Oguro <ingrid.shizue@gmail.com>
2026-01-15 17:47:50 -0300

    Movendo componente home para pasta pages

---
+++ src/app/components/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../bootstrap/navbar/navbar';
+import { Hero } from '../../sections/hero/hero';
+import { Experience } from '../../sections/experience/experience';
+import { Projects } from '../../sections/projects/projects';
+import { Contact } from '../../sections/contact/contact';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [Navbar,
+    Hero,
+    Experience,
+    Projects,
+    Contact
+      ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
