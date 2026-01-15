afe2da9e102870549e2cb771f1a0b5892028b292
IS Oguro <ingrid.shizue@gmail.com>
2026-01-15 17:38:12 -0300

    Componente experiência e adicionando padding top na home.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../bootstrap/navbar/navbar';
 import { Hero } from '../sections/hero/hero';
+import { Experience } from '../sections/experience/experience';
+
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, Experience],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
