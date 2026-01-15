a40348f850cfa1d3bbb51ecc06f34c37afe2d731
IS Oguro <ingrid.shizue@gmail.com>
2026-01-15 18:08:19 -0300

    Utilizando componente card

--- src/app/components/sections/projects/projects.ts
+++ src/app/components/sections/projects/projects.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Card } from '../../bootstrap/card/card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [Card],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
